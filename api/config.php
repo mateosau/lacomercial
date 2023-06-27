9c52e322d51796dc9241a669d1fbfd695bebfa59
Agustina Niz <agustinaniz@institutocedec.com>
2023-06-27 20:33:07 -0300

    API-REST CON PHP clase 27/06

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+/*Defininims las constantes de conexion */
+define('DB_HOST', 'localhost'); //nombre del servidor msyql
+define('DB_USER', 'root'); // usuario del servidor msyql
+define('DB_PASS', '');// contraseña del servidro msyql
+define('DB_NAME', 'ventas'); // nombre de la base de datos
+define('DB_CHARSET', 'utf8'); // cotejamiento del cojunto de caracteres 
+?>
