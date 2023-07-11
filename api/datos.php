941e3a683fb0e9636d3254d5b36c7f2e6e8a92da
Agustina Niz <agustinaniz@institutocedec.com>
2023-07-11 20:27:20 -0300

    listado desde la API clase 11/07

--- api/datos.php
+++ api/datos.php
@@ -1,7 +1,11 @@
 <?php
 require_once 'modelos.php'; // requerimos el archivo de clases modelo.php
 
-$tabla = new ModeloABM('articulos'); 
+if (isset($_GET['tabla'])) {
+    $t = $_GET['tabla'];
 
-$datos = $tabla->seleccionar();
-echo $datos;
+    $tabla = new ModeloABM('$t');
+
+    $datos = $tabla->seleccionar();
+    echo $datos;
+}
