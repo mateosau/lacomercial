d93245768604188e901c3b96f35e680d67efd955
mateo saucedo <mateosaucedo@institutocedec.com>
2023-09-12 20:23:38 -0300

    iniciar sesion clase del 12/09

---
+++ api/login.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'modelos.php'; // requerimos el archivo de clases modelo.php
+$mensaje = '';
+$valores = $_POST; // tomamos los valores que vienen del post en formato json
+if($_POST['usuario'] == '' || $_POST ['password'] == ''){
+    $mensaje .= "el usuario o la contraseña entan vacios<br>";
+    echo json_decode($mensaje);
+} else{
+    $usuario = "'" . $_POST['usuario'] . "'";
+    $password = "'" . $_POST['password'] . "'";
+
+    $usuario = new ModeloABM('clientes');
+    $usuario->set_criterio("usuario=$usuario AND password=$password");
+    $datos = $usuario->seleccionar();
+    if(count(json_decode($datos)) == 0 ){
+        $mensaje .= "el usuario o la contraseña no coinciden<br>";
+        echo json_decode($mensaje);
+    } else{
+        echo $datos;
+    }
+}
+?>
